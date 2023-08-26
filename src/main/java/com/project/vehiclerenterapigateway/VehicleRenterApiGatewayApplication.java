59adafa424103bf76ec6a56c9b59cb267aa5d23e
oenegm <oenegm@gmail.com>
2023-08-26 11:26:41 +0300

    init

---
+++ src/main/java/com/project/vehiclerenterapigateway/VehicleRenterApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.project.vehiclerenterapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRenterApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRenterApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/vehiclerenterapigateway/VehicleRenterApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vehiclerenterapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRenterApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
