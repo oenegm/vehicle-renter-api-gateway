900cbebac09864eddb2e369869af048b638f85c2
oenegm <oenegm@gmail.com>
2023-08-26 14:15:34 +0300

    fix

---
+++ src/main/kotlin/com/project/vehiclerenterapigateway/VehicleRenterApiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.project.vehiclerenterapigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VehicleRenterApiGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<VehicleRenterApiGatewayApplication>(*args)
+}
